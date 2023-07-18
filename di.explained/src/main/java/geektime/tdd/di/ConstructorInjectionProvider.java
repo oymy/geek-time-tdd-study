3403cd0c7e06cca0cd939b9d9130e50b42626caf
manyan.ouyang <manyan.ouyang@advancegroup.com>
2023-07-18 12:38:03 +0800

    work saving
    Signed-off-by: manyan.ouyang <manyan.ouyang@advancegroup.com>

--- di.explained/src/main/java/geektime/tdd/di/ConstructorInjectionProvider.java
+++ di.explained/src/main/java/geektime/tdd/di/ConstructorInjectionProvider.java
@@ -2,11 +2,10 @@ package geektime.tdd.di;
 
 import jakarta.inject.Inject;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Parameter;
+import java.lang.reflect.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,10 +20,36 @@ class ConstructorInjectionProvider<T> implements ContextConfig.ComponentProvider
     private final Constructor<T> injectConstructor;
 
     private final List<Field> injectFields;
+    private final List<Method> injectMethods;
+
 
     public ConstructorInjectionProvider(Class<T> component) {
         this.injectConstructor = getInjectConstructor(component);
         this.injectFields = getInjectFields(component);
+        this.injectMethods = getInjectMethods(component);
+    }
+
+    private static <T> List<Method> getInjectMethods(Class<T> component) {
+        List<Method> injectMethods = new ArrayList<>();
+        Class<?> current = component;
+        while (current != Object.class) {
+            injectMethods.addAll(stream(current.getDeclaredMethods()).filter(
+                            m -> m.isAnnotationPresent(Inject.class)
+                    ).filter(m -> injectMethods.stream().noneMatch(o ->
+                            o.getName().equals(m.getName()) &&
+                                    Arrays.equals(o.getParameterTypes(), m.getParameterTypes())
+                    ))
+                    .filter(m ->
+                            stream(component.getDeclaredMethods()).filter(
+                                            m1 -> !m1.isAnnotationPresent(Inject.class))
+                                    .noneMatch(m1 -> m1.getName().equals(m.getName()) &&
+                                            Arrays.equals(m1.getParameterTypes(), m.getParameterTypes()
+                                            )))
+                    .collect(Collectors.toList()));
+            current = current.getSuperclass();
+        }
+        Collections.reverse(injectMethods);
+        return injectMethods;
     }
 
     private static <T> List<Field> getInjectFields(Class<T> component) {
@@ -67,6 +92,14 @@ class ConstructorInjectionProvider<T> implements ContextConfig.ComponentProvider
             for (Field field : injectFields) {
                 field.set(instance, context.get(field.getType()).get());
             }
+            for (Method method : injectMethods) {
+                List<Object> objects = new ArrayList<>();
+                objects.addAll(stream(method.getParameterTypes()).map(
+                                it -> context.get(it).get()
+                        ).toList()
+                );
+                method.invoke(instance, objects.toArray());
+            }
 
             return instance;
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
@@ -76,8 +109,10 @@ class ConstructorInjectionProvider<T> implements ContextConfig.ComponentProvider
 
     @Override
     public List<Class<?>> getDependencies() {
-        return Stream.concat(stream(injectConstructor.getParameters()).map(Parameter::getType),
-                injectFields.stream().map(Field::getType)
+        return Stream.concat(Stream.concat(
+                        stream(injectConstructor.getParameters()).map(Parameter::getType),
+                        injectFields.stream().map(Field::getType)),
+                injectMethods.stream().flatMap(m -> stream(m.getParameterTypes()))
         ).toList();
     }
 }
